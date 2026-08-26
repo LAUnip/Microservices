3f643f3d2af8e777a4af971a7c9028bf7f113417
Luis Aguiar <luis.aguiar@tech.unipartner.com>
2026-08-26 14:08:05 +0100

    Initial commit

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,28 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo(AppDbContext context) : IPlatformRepo
+    {
+        public void CreatePlatform(Platform plat)
+        {
+            ArgumentNullException.ThrowIfNull(plat);
+            context.Platforms.Add(plat);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return context.Platforms.ToList();
+        }
+
+        public Platform? GetPlatformById(int id)
+        {
+            return context.Platforms?.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return context.SaveChanges() >= 0;
+        }
+    }
+}
