39d485bbc5d6cee28e595e3954a66a61cc850fda
Luis Aguiar <luis.aguiar@tech.unipartner.com>
2026-08-26 14:57:53 +0100

    Added GetById & Create

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -17,6 +18,30 @@ namespace PlatformService.Controllers
             var platforms = repository.GetAllPlatforms();
 
             return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("--> Getting Platform by Id...");
+            var platform = repository.GetPlatformById(id);
+            if (platform is null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<PlatformReadDto>(platform));
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            Console.WriteLine("--> Creating Platform...");
+            var platformModel = mapper.Map<Platform>(platformCreateDto);
+            repository.CreatePlatform(platformModel);
+            repository.SaveChanges();
+            var platformReadDto = mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtAction(nameof(GetPlatformById), new { platformReadDto.Id }, platformReadDto);
+
         }
     }
 }
